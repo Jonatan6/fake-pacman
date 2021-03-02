0e316c1edd1b6a3ea7731df68de0d92ec36c285a
Jonatan Schneevoigt <jonatan.gud.hum@gmail.com>
2021-03-02 13:29:20 +0100

    Make the typer function more reliable

--- typer.h
+++ typer.h
@@ -1,10 +1,10 @@
-int typer(int y, int x, bool hidden, char* pacupdate, char* usrupdate) {
-  for (int ch; ch != 10; x++) {
+int typer(int y, int x, bool hidden, char* pacupdate) {
+  for (int ch, ox = x; ch != 10; x++) {
     ch = getch();
     if (ch != 127) {
       if (!hidden) mvprintw(y, x, "%c", ch);
       else mvprintw(y, x, "*");
-      if (pacupdate) pacupdate[x - 28 - strlen(usrupdate)] = ch;
+      if (pacupdate) pacupdate[x - ox] = ch;
     }
     else x = x - 2;
     mvprintw(y, x + 1, " ");	
